cf6e99d57704c52f0ae2614c9d1de3ed13bbb69b
tygdcf <anaelle.barthel@gmail.com>
2026-02-27 10:21:19 +0100

    Add files via upload

---
+++ data/Refreshment_9.js
@@ -0,0 +1,1 @@
+var json_Refreshment_9 = {"type":"FeatureCollection","name":"Refreshment_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1"},"geometry":{"type":"Point","coordinates":[5.019409763695381,51.344759247783372]}}]}
